cf45d53f87cd3bbe6138aed839c1fbfd272b8527
Victor <victormonteiromaia95@gmail.com>
2019-09-15 21:07:33 -0300

    15-09-2019

--- phpoo/poo1.php
+++ phpoo/poo1.php
@@ -1,4 +1,33 @@
 <?php
 
+    class Pessoa{
+        
+        public $nome;
+        
+        public function falar(){
+                return "meu nome eh Victor Rangel Monteiro Maia".$this->nome;
+        }
+    }
 
+    $victor = new Pessoa();
+    $victor ->nome="Qual eh meu nome";
+    echo $victor->falar();
+
+
+    class Time{
+        public $clube="flamengo";
+        public function torce(){
+            return $this->clube."meu time eh";
+        }
+
+    }
+
+    echo "<br><br>";
+
+    $gabriel = new time();
+    $gabriel->clube;
+    echo $gabriel->torce().$victor->falar();
 ?>
+
+
+
