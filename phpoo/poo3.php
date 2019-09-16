a729693d8206a33aa4f0f1580286b1d6ef88cf16
Victor <victormonteiromaia95@gmail.com>
2019-09-16 19:40:16 -0300

    16-09-19

--- phpoo/poo3.php
+++ phpoo/poo3.php
@@ -1,5 +1,46 @@
 <?php
 
+        class Carro{
+
+           private $modelo;
+           private $motor;
+           private $ano;
+           
+           public function getModelo(){
+               return $this->modelo;
+           }
+           public function setModelo($modelo){
+                $this->modelo=$modelo;
+           }
+
+            public function getMotor(){
+                return $this->motor;
+            }
+            public function setMotor($motor){
+                $this->motor=$motor;
+            }
+            public function getAno(){
+                return $this->ano;
+            }
+            public function setAno($ano){
+                $this->ano=$ano;
+            }
+
+            public function getInfo(){
+                array(
+                    "modelo"=>$this->getModelo(),
+                    "motor"=>$this->getMotor(),
+                    "ano"=>$this->getAno()
+                );
+            }
+    }
+
+    $veiculo = new Carro();
+    $veiculo->setModelo("GOL");
+    $veiculo->setMotor("motor box");
+    $veiculo->setAno("80");
+    
+    var_dump($veiculo->getInfo());
 
 
 ?>
